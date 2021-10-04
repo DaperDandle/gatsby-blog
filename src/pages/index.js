c8e1bd96b3d65fafbd1998ef50b2d43e84fa73a5
Daniel Weber <1678401@park.edu>
2021-10-03 23:19:00 -0500

    Added gatsby-plugin-image and image on home page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,14 @@
 import * as React from "react";
 import Layout from "../components/layout";
+import { StaticImage } from "gatsby-plugin-image";
 
 // markup
 const IndexPage = () => {
   return (
     <Layout title="Home Page">
       <h1>Welcome to my Gatsby Site</h1>
+
+      <StaticImage src="../images/nice-guy.jpg" alt="some guy" />
 
       <p>I'm making this following the Gatsby tutorial!</p>
     </Layout>
