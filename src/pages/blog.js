e3d540c868e153fd733e3095a6d0d604d1f28afe
Daniel Weber <1678401@park.edu>
2021-10-05 00:12:36 -0500

    pulled blog posts from graphql into blog page

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,24 +1,39 @@
 import React from "react";
 import Layout from "../components/layout";
 import { graphql } from "gatsby";
+import { MDXRenderer } from "gatsby-plugin-mdx";
 
 const BlogPage = ({ data }) => {
   return (
     <Layout title="Blog Posts">
-      <ul>
-        {data.allFile.nodes.map((node) => {
-          return <li key={node.name}>{node.name}</li>;
-        })}
-      </ul>
+      {data.allMdx.nodes.map((node) => {
+        const {
+          id,
+          frontmatter: { title, date },
+          body,
+        } = node;
+        return (
+          <article key={id}>
+            <h2>{title}</h2>
+            <h3>Posted: {date}</h3>
+            <MDXRenderer>{body}</MDXRenderer>
+          </article>
+        );
+      })}
     </Layout>
   );
 };
 
 export const query = graphql`
-  query MyQuery {
-    allFile {
+  query {
+    allMdx(sort: { fields: frontmatter___date, order: DESC }) {
       nodes {
-        name
+        frontmatter {
+          date
+          title
+        }
+        id
+        body
       }
     }
   }
